7f4f63fe0f240322e039a1472e8071e6c3d8bda5
JuanB-18 <jua.broncano@duocuc.cl>
2024-06-19 15:41:11 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,13 @@
-#este es el archivo python donde trabajaremos de manera colaborativa
+def sumar (num1,num2):
+    resultado= num1+num2
+    return resultado
+def restar(num1,num2):
+    resultado= num1-num2
+    return resultado
+def multiplicar(num1,num2):
+    resultado= num1*num2
+    return resultado
+def dividir(num1,num2):
+    resultado= num1/num2
+    return resultado
+numero1=int(input("Ingrese un numero:\n"))
